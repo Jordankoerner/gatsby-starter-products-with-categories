d96e2e4bb4e1b9991440d10d3f7b871958a9b947
Jordankoerner <jordankoerner@gmail.com>
2020-06-14 02:09:30 -0700

    c

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,16 @@ class IndexPost extends React.Component {
   render() {
 
     const { data } = this.props;
-    console.log(data);
 
     return (
       <React.Fragment>
         <div className="row product-main">
-          {data.data.allCategory.nodes.map(items => (
-            <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={items.id}>
+          {data.data.allProduct.edges.map(items => (
+            <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={items.node.id}>
               <div className="details_List">
 
-                  { items.image && items.image[0] ? <Img sizes={{
-                      "src": `${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.image[0].id}.${items.image[0].extension}`,
+                  { items.node.productImage && items.node.productImage[0] ? <Img sizes={{
+                      "src": `${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.node.productImage[0].id}.${items.node.productImage[0].extension}`,
                       "aspectRatio": 1.77,
                       "sizes": '',
                       "srcSet": ''
@@ -29,8 +28,26 @@ class IndexPost extends React.Component {
                   <div className="details_inner">
 
                   <h2>
-                    <Link to={`/${items.slug}`}>{items.name}</Link>
+                    <Link to={`/${items.node.slug}`}>{items.node.name}</Link>
                   </h2>
+                  <div className="row">
+                    <div className="col-sm-4 align-self-center">
+                      <span className="price">${items.node.price}</span>
+                    </div>
+                    <div className="col-sm-8 text-right align-self-center">
+                      <a
+                        href="/"
+                        className="Product snipcart-add-item"
+                        data-item-id={items.node.slug}
+                        data-item-price={items.node.price}
+                        data-item-image={items.node.productImage && items.node.productImage[0] ? `${process.env.GATSBY_FLOTIQ_BASE_URL}/image/1920x0/${items.node.productImage[0].id}.${items.node.productImage[0].extension}` : ""}
+                        data-item-name={items.node.name}
+                        data-item-url={`/`}
+                      >
+                        <i className="fas fa-shopping-bag" />Add to Cart
+                    </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -46,8 +63,8 @@ const IndexPage = data => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div className="container">
-      <div className="text-center mt-5"><h2 className="with-underline">Categories</h2></div>
-      <IndexPost data={data} />
+      <div className="text-center mt-5"><h2 className="with-underline">All Items</h2></div>
+      <IndexPost data={data}></IndexPost>
     </div>
   </Layout>
 )
@@ -56,17 +73,25 @@ export default IndexPage
 
 export const query = graphql`
   query AboutQuery {
-    allCategory(sort: {fields: flotiqInternal___createdAt, order: DESC}) {
-    nodes {
-      id
-      image {
-        extension
-        id
+    allProduct(sort: {fields: flotiqInternal___createdAt, order: DESC}) {
+      edges{
+        node{
+          id
+          slug,
+          name,
+          price,
+          description,
+          productImage {
+             id,
+             extension
+          },
+          productGallery {
+             id,
+             extension
+          }
+        }
       }
-      name
-      slug
     }
-  }
   }
 `
 
